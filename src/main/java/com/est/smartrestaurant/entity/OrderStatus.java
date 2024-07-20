59516881c56c5a08fd57c72c9498228aae2d745c
hyezuu <duudu0704@gmail.com>
2024-07-20 23:10:12 +0900

    :recycle: update OrderStatus

--- src/main/java/com/est/smartrestaurant/entity/OrderStatus.java
+++ src/main/java/com/est/smartrestaurant/entity/OrderStatus.java
@@ -4,9 +4,9 @@ import lombok.Getter;
 
 @Getter
 public enum OrderStatus {
-    ORDER_REQUEST(1, "주문 접수"),
-    ORDER_COMPLETE(2, "주문 완료"),
-    ORDER_CANCEL(3, "주문 취소");
+    ORDER_RECEIVED(1, "주문 접수"),
+    ORDER_COMPLETED(2, "주문 완료"),
+    ORDER_CANCELED(3, "주문 취소");
 
     private int stepNumber;
     private String stepDescription;
