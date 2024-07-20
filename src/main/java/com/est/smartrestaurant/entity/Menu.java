2e0efd7c62a3eaa20dc6839e143c81062963ff11
hyezuu <duudu0704@gmail.com>
2024-07-20 19:02:47 +0900

    :sparkles: Menu Entity

--- src/main/java/com/est/smartrestaurant/entity/Menu.java
+++ src/main/java/com/est/smartrestaurant/entity/Menu.java
@@ -1,5 +1,52 @@
 package com.est.smartrestaurant.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class Menu {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+    @Column(nullable = false)
+    private String category;
+    @Column(nullable = false)
+    private Double price;
+    @Column(nullable = false)
+    private String description;
+
+    @Builder
+    private Menu(String name, String category, Double price, String description) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.description = description;
+    }
+
+    public void updateName(String name) {
+        this.name = name;
+    }
+
+    public void updateCategory(String category) {
+        this.category = category;
+    }
+
+    public void updatePrice(Double price) {
+        this.price = price;
+    }
+
+    public void updateDescription(String description) {
+        this.description = description;
+    }
 }
