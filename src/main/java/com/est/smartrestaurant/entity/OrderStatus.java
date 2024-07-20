3076174584c123b880bdd1302df3f90b3154157a
hyezuu <duudu0704@gmail.com>
2024-07-20 23:11:45 +0900

    :recycle: update OrderStatus

--- src/main/java/com/est/smartrestaurant/entity/OrderStatus.java
+++ src/main/java/com/est/smartrestaurant/entity/OrderStatus.java
@@ -8,8 +8,8 @@ public enum OrderStatus {
     ORDER_COMPLETED(2, "주문 완료"),
     ORDER_CANCELED(3, "주문 취소");
 
-    private int stepNumber;
-    private String stepDescription;
+    private final int stepNumber;
+    private final String stepDescription;
 
     OrderStatus(int stepNumber, String stepDescription) {
         this.stepNumber = stepNumber;
