bfe5aa7a8c14927eb8feff6ed70cb6a4214a7abc
hyezuu <duudu0704@gmail.com>
2024-07-20 19:54:26 +0900

    :recycle: update Entity name

--- src/main/java/com/est/smartrestaurant/entity/Order.java
+++ src/main/java/com/est/smartrestaurant/entity/Order.java
@@ -1,5 +1,28 @@
 package com.est.smartrestaurant.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity(name = "orders")
+@NoArgsConstructor
 public class Order {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus = OrderStatus.ORDER_REQUEST;
 }
