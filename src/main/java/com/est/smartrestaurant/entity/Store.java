bae4046a62ec65905a7f90f79ab502aa3ff57ec4
hyezuu <duudu0704@gmail.com>
2024-07-20 19:50:02 +0900

    :sparkles: Store Entity

--- src/main/java/com/est/smartrestaurant/entity/Store.java
+++ src/main/java/com/est/smartrestaurant/entity/Store.java
@@ -1,5 +1,45 @@
 package com.est.smartrestaurant.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity(name = "stores")
+@NoArgsConstructor
 public class Store {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+    @Column(nullable = false)
+    private String address;
+
+    @Builder
+    private Store(String name, String phoneNumber, String address) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public void updateName(String newName) {
+        this.name = newName;
+    }
+
+    public void updatePhoneNumber(String newPhoneNumber) {
+        this.phoneNumber = newPhoneNumber;
+    }
+
+    public void updateAddress(String newAddress) {
+        this.address = newAddress;
+    }
 }
