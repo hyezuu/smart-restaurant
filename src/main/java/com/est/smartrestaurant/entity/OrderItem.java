4df447ec827d65b6cd4b189bd56b4c94c8fe5290
hyezuu <duudu0704@gmail.com>
2024-07-20 19:54:58 +0900

    :recycle: update relationship

--- src/main/java/com/est/smartrestaurant/entity/OrderItem.java
+++ src/main/java/com/est/smartrestaurant/entity/OrderItem.java
@@ -1,5 +1,28 @@
 package com.est.smartrestaurant.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class OrderItem {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "menu_id")
+    private Menu menu;
 }
