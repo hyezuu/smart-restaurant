2135bb1338d21b970b81b48aee18600b13d2d776
hyezuu <duudu0704@gmail.com>
2024-07-23 21:57:40 +0900

    :recycle: update CustomerDTO

--- src/main/java/com/est/smartrestaurant/domain/dto/CustomerDTO.java
+++ src/main/java/com/est/smartrestaurant/domain/dto/CustomerDTO.java
@@ -1,12 +1,15 @@
 package com.est.smartrestaurant.domain.dto;
 
+import com.est.smartrestaurant.common.validation.NotSpace;
 import com.est.smartrestaurant.domain.entity.Customer;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public class CustomerDTO {
+
     private final static String DEFAULT_PATTERN_MESSAGE =
         "휴대폰 번호는 010으로 시작하는 11자리 숫자와 '-'로 구성되어야 합니다";
+
     public record Post(
         @NotBlank(message = "이름은 공백일 수 없습니다.")
         String name,
@@ -25,11 +28,10 @@ public class CustomerDTO {
         }
     }
 
-
     public record Patch(
         @Pattern(regexp = "^010-\\d{3,4}-\\d{4}$", message = DEFAULT_PATTERN_MESSAGE)
         String phoneNumber,
-        @NotBlank(message = "주소는 공백일 수 없습니다.")
+        @NotSpace(message = "주소는 공백일 수 없습니다.")
         String address
     ) {
 
@@ -47,6 +49,7 @@ public class CustomerDTO {
         String phoneNumber,
         String address
     ) {
+
         public static Response from(Customer customer) {
             return new Response(
                 customer.getId(),
@@ -56,5 +59,4 @@ public class CustomerDTO {
             );
         }
     }
-
 }
