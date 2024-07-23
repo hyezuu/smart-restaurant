e5660fff73c3f100e7b2cbab32aebe3b7edc4a97
hyezuu <duudu0704@gmail.com>
2024-07-24 05:46:35 +0900

    :recycle: update CustomException

--- src/main/java/com/est/smartrestaurant/common/exception/BusinessLogicException.java
+++ src/main/java/com/est/smartrestaurant/common/exception/BusinessLogicException.java
@@ -2,7 +2,7 @@ package com.est.smartrestaurant.common.exception;
 
 import lombok.Getter;
 
-public class ResourceConflictException extends RuntimeException {
+public class BusinessLogicException extends RuntimeException {
     public ResourceConflictException(ExceptionCode exceptionCode) {
         super(exceptionCode.getMessage());
     }
