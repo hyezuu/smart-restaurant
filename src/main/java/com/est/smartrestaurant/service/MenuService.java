45c6e3677740c984f07bf5dd98a632eb2983b7fe
hyezuu <duudu0704@gmail.com>
2024-07-24 07:01:07 +0900

    :recycle: update MenuService

--- src/main/java/com/est/smartrestaurant/service/MenuService.java
+++ src/main/java/com/est/smartrestaurant/service/MenuService.java
@@ -1,8 +1,7 @@
 package com.est.smartrestaurant.service;
 
+import com.est.smartrestaurant.common.exception.BusinessLogicException;
 import com.est.smartrestaurant.common.exception.ExceptionCode;
-import com.est.smartrestaurant.common.exception.ResourceConflictException;
-import com.est.smartrestaurant.common.exception.ResourceNotFoundException;
 import com.est.smartrestaurant.domain.entity.Menu;
 import com.est.smartrestaurant.repository.MenuRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,7 +19,7 @@ public class MenuService {
 
     public Menu save(Menu menu) {
         if(menuRepository.existsByName(menu.getName())) {
-            throw new ResourceConflictException(ExceptionCode.CUSTOMER_ALREADY_EXISTS);
+            throw new BusinessLogicException(ExceptionCode.MENU_ALREADY_EXISTS);
         }
         return menuRepository.save(menu);
     }
@@ -33,7 +32,7 @@ public class MenuService {
     @Transactional(readOnly = true)
     public Menu findById(Long id) {
         return menuRepository.findById(id).orElseThrow(()
-            -> new ResourceNotFoundException(ExceptionCode.MENU_NOT_FOUND));
+            -> new BusinessLogicException(ExceptionCode.MENU_NOT_FOUND));
     }
 
     public Menu update(Long id, Menu newMenu){
@@ -50,7 +49,7 @@ public class MenuService {
 
     public void delete(Long id) {
         if(!menuRepository.existsById(id)) {
-            throw new ResourceNotFoundException(ExceptionCode.MENU_NOT_FOUND);
+            throw new BusinessLogicException(ExceptionCode.MENU_NOT_FOUND);
         }
         menuRepository.deleteById(id);
     }
