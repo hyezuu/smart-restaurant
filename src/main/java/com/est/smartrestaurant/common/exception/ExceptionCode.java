32e92a3eff0438d29f02da63b5298561bb01ad16
hyezuu <duudu0704@gmail.com>
2024-07-25 09:26:59 +0900

    :recycle: update ExceptionCode

--- src/main/java/com/est/smartrestaurant/common/exception/ExceptionCode.java
+++ src/main/java/com/est/smartrestaurant/common/exception/ExceptionCode.java
@@ -9,7 +9,10 @@ public enum ExceptionCode {
     CUSTOMER_NOT_FOUND(404, "Customer not found"),
     CUSTOMER_ALREADY_EXISTS(409,"Customer already exists"),
     MENU_NOT_FOUND(404, "Menu not found"),
-    MENU_ALREADY_EXISTS(409, "Menu already exists");
+    MENU_ALREADY_EXISTS(409, "Menu already exists"),
+    STORE_NOT_FOUND(404, "Store not found"),
+
+    STORE_ALREADY_EXISTS(409, "Store already exists");
 
     public final int status;
     public final String message;
